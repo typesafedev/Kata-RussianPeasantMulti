969ff4e88d7ac99e95bda7025dcba7a2290ea216
AndyLam <alam@neworbit.co.uk>
2018-05-28 21:44:50 +0100

    Add another version of Multiply() Add performance unit tests

---
+++ RussianPeasantMultiplication/RussianPeasantMultiplication/FunctionalMultiplier.cs
@@ -0,0 +1,64 @@
+﻿namespace RussianPeasantMultiplication
+{
+    using System;
+    using System.Collections.Generic;
+    using static System.Console;
+    using static System.Math;
+
+    //Russian peasant multiplication wriiten in more functional style.
+    public static class Functional
+    {
+        private static IEnumerable<(int x, int y)> Unfold(int x, int y, Action<int, int> print)
+        {
+            var x1 = x;
+            var y1 = y;
+
+            while (true)
+            {
+                print(x1, y1);
+                if (y1 == 0) { yield break; }
+                yield return (x1, y1);
+                x1 *= 2;
+                y1 /= 2;
+            }
+        }
+
+        private static int Fold(IEnumerable<(int x, int y)> terms, Action<int> print)
+        {
+            var acc = 0;
+            foreach (var (x, y) in terms)
+            {
+                //sum only if y is odd
+                if (y % 2 != 0) { acc += x; }
+            }
+            print(acc);
+            return acc;
+        }
+
+        private static void Print(int x, int y)
+        {
+            if (y % 2 == 0) { ForegroundColor = ConsoleColor.Red; }
+            else { ForegroundColor = ConsoleColor.Green; }
+
+            WriteLine($"{x} {y}");
+        }
+
+        private static void Print(int product)
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine($"\nResult = {product}");
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            var products = Unfold(x, y, Print);
+            var product = Fold(products, Print);
+
+            //Normalize product
+            if (x > 0 == y > 0) { return Abs(product); }
+
+            return -Abs(product);
+        }
+
+    }
+}
---
+++ RussianPeasantMultiplication/RussianPeasantMultiplication/PerformanceTests.cs
@@ -0,0 +1,25 @@
+﻿namespace RussianPeasantMultiplication
+{
+    using FluentAssertions;
+    using System;
+    using Xunit;
+
+    //Questionable utility. Test machine will have to be stable for this to work.
+    public class PerformanceTests
+    {
+        [Fact]
+        public void ObjectOrientedMultiply()
+        {
+            var sut = new RussianPeasantMultiplier();
+            sut.ExecutionTimeOf(s => s.Multiply(1000, 1000)).Should().BeLessThan(TimeSpan.FromMilliseconds(100));
+        }
+
+        [Fact]
+        public void FunctionalMultiply()
+        {
+            Action action = () => Functional.Multiply(1000, 1000);
+            action.ExecutionTime().Should().BeLessThan(TimeSpan.FromMilliseconds(100));
+        }
+
+    }
+}
